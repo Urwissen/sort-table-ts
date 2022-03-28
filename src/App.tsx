c4524efa358201fbd24265b5318a9d53c06db16e
Lars <l.gessner@posteo.de>
2022-03-28 16:26:29 +0200

    add sort logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Table from './components/Table';
 
 function App() {
   const [userList, setUserList] = React.useState([])
+  const [sort, setSort] = React.useState(false)
 
   const url: string = "https://jsonplaceholder.typicode.com/users"
 
@@ -14,18 +15,49 @@ function App() {
     .then((data) => setUserList(data))
     .catch((error) => console.log(error))
   }, [])
-  console.log(userList)
+  console.log(typeof userList)
 
-  interface User {
-    adress: {},
-    company: {},
-    email: string,
-    id: number,
-    name: string,
-    phone: string,
-    username: string,
-    website: string,
-  }
+  function handleSort(e: any){
+    setSort(prev => !prev)
+    const clickedTitle: string = e.target.textContent.toLowerCase()
+    if (clickedTitle in userList[0]) {
+      console.log("title " + clickedTitle +" is in userlist")
+      if (clickedTitle && sort === false) {
+        console.log(clickedTitle)
+        setUserList(prevList => {
+          return prevList.sort((a: any,b: any) => {
+            if (a[clickedTitle] > b[clickedTitle]) {
+              return -1;
+            }
+            if (a[clickedTitle] < b[clickedTitle]) {
+              return 1;
+            }
+            return 0;
+          })  
+        })
+      }
+      if (clickedTitle && sort) {
+        console.log(clickedTitle)
+        console.log("before", userList)
+        setUserList(prevList => {
+          return prevList.sort((a: any,b: any) => {
+            if (a[clickedTitle] < b[clickedTitle]) {
+              return -1;
+            }
+            if (a[clickedTitle] > b[clickedTitle]) {
+              return 1;
+            }
+            return 0;
+          })  
+        })
+      }
+      console.log("after",userList)
+    } else {
+      alert("unable to sort by "+ clickedTitle)
+    }
+    console.log("before", userList)
+    
+}
 
   return (
     <div className="App">
@@ -44,8 +76,7 @@ function App() {
         </a>
       </header>
       <main>
-        <Table users={userList}/>
-        {userList.map((user: User) => <p key={user.id}>{user.name}</p>)}
+        <Table users={userList} handler={handleSort}/>
       </main>
     </div>
   );
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,10 +1,43 @@
 import React from 'react'
+interface User {
+    address: {},
+    company: {},
+    email: string,
+    id: number,
+    name: string,
+    phone: string,
+    username: string,
+    website: string,
+}
+interface Props {
+    users: User[],
+    handler: Function,
+}
 
-function Table() {
+function Table({users,handler }: Props) {
     return (
-    <div>Table
+    <table>
+        <thead>
+            <th onClick={(e) => handler(e)}>Name</th>
+            <th onClick={(e) => handler(e)}>Username</th>
+            <th onClick={(e) => handler(e)}>Email</th>
+            <th onClick={(e) => handler(e)}>Website</th>
+        </thead>
         
-    </div>
+        {users.map((user: User) => {
+            return(
+                <tbody>
+                    <td key={user.id}>{user.name} </td>
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td>{user.website}</td>
+                </tbody>
+            )
+            }
+        )}
+        
+
+    </table>
     )
 }
 
